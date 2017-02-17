70e39f5ebc03c34411481352338bc0903042b222
tusharkoul <tusharkoul@gmail.com>
2017-02-16 22:38:55 -0800

    adding signup view and its route

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('loginview', { title: 'Express' });
+    res.render('loginview');
 });
 
 router.get('/login', function(req, res, next) {
-    res.render('loginview', { title: 'Express' });
+    res.render('loginview');
+});
+
+router.get('/emailsignup', function(req, res, next) {
+    res.render('emailsignup');
 });
 
 router.get('/matchlist', function(req, res, next) {

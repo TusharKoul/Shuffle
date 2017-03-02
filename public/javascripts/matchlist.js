eb8d1da16b699c0e6d8348cb4ebf526e4849c009
tusharkoul <tusharkoul@gmail.com>
2017-03-02 03:23:31 -0800

    adding spinner ui while loading waveform

--- public/javascripts/matchlist.js
+++ public/javascripts/matchlist.js
@@ -10,85 +10,9 @@ $(document).ready(function(){
     setupPlayPauseHandler();
 });
 
-function incrementChatProgress(userId) {
-    var chatButton = $('#chatButton'+userId);
-    chatButton.progressIncrement();
-}
 
-function decrementChatProgress(userId) {
-    var chatButton = $('#chatButton'+userId);
-    chatButton.progressDecrement();
-}
-
-function setupSongsForUser(userId) {
-    var url = '/songsjson/' + userId;
-    $('.songlist').html('<p>LOADING ...</p>');
-    $.get(url, function(songlist){
-        $('.songlist').html('');
-        // Adding individual songs containers
-        var htmlStr = '';
-        var likedCount = 0;
-        for(i = 0; i< songlist.length; i++) {
-            htmlStr = getSongContainerHtml(songlist[i]);
-            $('.songlist').append(htmlStr);
-
-            setupSongwaves(songlist[i]);
-
-            if (songlist[i].liked) {
-                likedCount += 1;
-            }
-        }
-
-        // Based on 'likes' setup chat button loading ratio
-        var chatButton = $('#chatButton'+userId);
-        chatButton.progressSet(likedCount*34);
-    });
-}
-
-function setupLikeHandler() {
-    $(document).on("click", ".glyphicon-heart-empty", function(event){
-        $(this).toggleClass("glyphicon-heart-empty glyphicon-heart");
-        postToggleLikeOnSong($(this).attr('id'),1);
-        incrementChatProgress(currentUserId);
-    });
-
-    $(document).on("click", ".glyphicon-heart", function(event){
-        $(this).toggleClass("glyphicon-heart-empty glyphicon-heart");
-        postToggleLikeOnSong($(this).attr('id'), 0);
-        decrementChatProgress(currentUserId);
-    });
-}
-
-function postToggleLikeOnSong(songid,liked) {
-    $.ajax({
-        url: '/togglelike',
-        type: 'PUT',
-        data: {songid: songid, liked : liked},
-        success:function(data){
-            console.log('put success' + data);
-        }
-    });
-}
-
-function setupPlayPauseHandler() {
-    $(document).on("click", ".glyphicon-pause", function(event){
-        $(this).toggleClass("glyphicon-pause glyphicon-play");
-        var songid = $(this).attr('id').replace('song-playpause','');
-        togglePlayPause(songid);
-    });
-
-    $(document).on("click", ".glyphicon-play", function(event){
-        $(this).toggleClass("glyphicon-pause glyphicon-play");
-        var songid = $(this).attr('id').replace('song-playpause','');
-        togglePlayPause(songid);
-    });
-}
-
-function togglePlayPause(songid) {
-    var songwave = currentSongwaveDict[songid];
-    songwave.playPause();
-}
-
+// SLIDER RELATED METHODS
+// **************************
 
 function setupSlider(){
     $('.slider').slick({
@@ -132,6 +56,40 @@ function handleSliderEvents() {
 }
 
 
+// SONG CONTAINER RELATED METHODS
+// ******************************
+
+
+function setupSongsForUser(userId) {
+    $('.songlist').html(getLoadingHtml(true,5));
+    
+    // setTimeout(function () {
+        $.get('/songsjson/' + userId, function(songlist) {
+
+            // Clear song list container element
+            $('.songlist').html('');
+
+            // Adding individual songs containers
+            var htmlStr = '';
+            var likedCount = 0;
+            for(i = 0; i< songlist.length; i++) {
+                htmlStr = getSongContainerHtml(songlist[i]);
+                $('.songlist').append(htmlStr);
+
+                setupSongwaves(songlist[i]);
+
+                if (songlist[i].liked) {
+                    likedCount += 1;
+                }
+            }
+
+            // Based on 'likes' setup chat button loading ratio
+            var chatButton = $('#chatButton'+userId);
+            chatButton.progressSet(likedCount*34);
+        });
+    // },1200);
+}
+
 
 function getSongContainerHtml(songJson) {
     var liked = songJson.liked;
@@ -164,18 +122,95 @@ function getSongContainerHtml(songJson) {
     return str;
 }
 
-function getLoadingHtml() {
-    var str = '<div class="spinner">';
-    for(var i=0; i<8; i++) {
-        str += '<div class="rect' + i + '"></div>';
+function getLoadingHtml(withContainer,len) {
+    var str = '';
+    if(withContainer) {
+        str += '<div class="spinner-container">'
+    }
+    str += '<div class="spinner">';
+    for(var i=0; i<len; i++) {
+        str += '<div class="rect' + (i+1) + '"></div>';
     }
     str += '</div>';
+    if(withContainer) {
+        str += '</div>';
+    }
     return str
 }
 
 
+// SONG LIKE RELATED METHODS
+// ******************************
+
+
+function setupLikeHandler() {
+    $(document).on("click", ".glyphicon-heart-empty", function(event){
+        $(this).toggleClass("glyphicon-heart-empty glyphicon-heart");
+        postToggleLikeOnSong($(this).attr('id'),1);
+        incrementChatProgress(currentUserId);
+    });
+
+    $(document).on("click", ".glyphicon-heart", function(event){
+        $(this).toggleClass("glyphicon-heart-empty glyphicon-heart");
+        postToggleLikeOnSong($(this).attr('id'), 0);
+        decrementChatProgress(currentUserId);
+    });
+}
+
+function incrementChatProgress(userId) {
+    var chatButton = $('#chatButton'+userId);
+    chatButton.progressIncrement();
+}
+
+function decrementChatProgress(userId) {
+    var chatButton = $('#chatButton'+userId);
+    chatButton.progressDecrement();
+}
+
+
+function postToggleLikeOnSong(songid,liked) {
+    $.ajax({
+        url: '/togglelike',
+        type: 'PUT',
+        data: {songid: songid, liked : liked},
+        success:function(data){
+            console.log('put success' + data);
+        }
+    });
+}
+
+
+// SONG PLAY-PAUSE RELATED METHODS
+// *******************************
+
+
+function setupPlayPauseHandler() {
+    $(document).on("click", ".glyphicon-pause", function(event){
+        $(this).toggleClass("glyphicon-pause glyphicon-play");
+        var songid = $(this).attr('id').replace('song-playpause','');
+        togglePlayPause(songid);
+    });
+
+    $(document).on("click", ".glyphicon-play", function(event){
+        $(this).toggleClass("glyphicon-pause glyphicon-play");
+        var songid = $(this).attr('id').replace('song-playpause','');
+        togglePlayPause(songid);
+    });
+}
+
+function togglePlayPause(songid) {
+    var songwave = currentSongwaveDict[songid];
+    songwave.playPause();
+}
+
+
+// SONG WAVEFORM RELATED METHODS
+// *******************************
+
+
 function setupSongwaves(songJson) {
     var songwaveid = '#song-waveform' + songJson.songid;
+    showLoadingSoundwave(songwaveid);
 
     var wavesurfer = WaveSurfer.create({
         container: songwaveid,
@@ -188,10 +223,11 @@ function setupSongwaves(songJson) {
     });
 
     wavesurfer.on('ready', function () {
-        console.log('ready');
-        // wavesurfer.play();
+        removeLoadingSoundwave();
+        console.log('ready' + songwaveid);
         currentSongwaveDict[songJson.songid] = wavesurfer;
     });
+
 
     $(document).on("destroy-songwaves", function () {
         wavesurfer.destroy();
@@ -200,6 +236,15 @@ function setupSongwaves(songJson) {
 
     // Hard coding song for now
     wavesurfer.load('../assets/song1.mp3');
+}
+
+function showLoadingSoundwave(songwaveid) {
+    $(songwaveid).html(getLoadingHtml(false,8));
+}
+
+function removeLoadingSoundwave() {
+    $('.spinner-container').remove();
+    $('.spinner').remove();
 }
 
 function destroyCurrentSongwaves() {

a6abbfbc60f4f47413b6554503c4701fdcd47010
tusharkoul <tusharkoul@gmail.com>
2017-03-08 14:58:40 -0800

    changes to onboarding screen

--- public/javascripts/onboarding.js
+++ public/javascripts/onboarding.js
@@ -1,17 +1,32 @@
 $(document).ready(function(){
 	$('#sel2').hide();
 
-	$('#sel1').change(function () {
-	    var selectedText = $(this).find("option:selected").text();
-	    console.log(selectedText);
+	handleDropdown1Selection();
+    handleNextClicked();
 
-	    if(selectedText === "I am"){
-	        $('#sel2').hide();
-	        $('#sel3').show();
-	    }
-	    else if(selectedText === "We are") {
-	    	$('#sel3').hide();
-		    $('#sel2').show();
-	    }    	
-	});
 });
+
+function handleDropdown1Selection() {
+    $('#sel1').change(function () {
+        var selectedText = $(this).find("option:selected").text();
+
+        if(selectedText === "I am"){
+            $('#sel2').hide();
+            $('#sel3').show();
+        }
+        else if(selectedText === "We are") {
+            $('#sel3').hide();
+            $('#sel2').show();
+        }
+    });
+}
+
+function handleNextClicked() {
+    $('.float-button').on('click', function (event) {
+        if ($(this).hasClass('float-button-disabled')) {
+            return;
+        }
+
+        window.location.href='/genreselection';
+    })
+}
